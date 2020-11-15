422ac38aa692e7629d03d5b997fd69be6527d0ca
Will Beebe <will@containerlabs.io>
2020-11-15 15:30:45 -0800

    v0

---
+++ main.tf
@@ -0,0 +1,37 @@
+# Copyright YYYY Google LLC
+#
+# This software is provided as-is, without warranty or representation for any
+# use or purpose. Your use of it is subject to your agreement with Google.
+
+# repo creation not currently managed by TF
+data "github_repository" "the_repo" {
+  full_name = "${var.github_org}/${var.repo_name}"
+}
+
+resource "github_membership" "membership_for_some_user" {
+  for_each = toset(var.members)
+
+  username = each.key
+  role     = "member"
+}
+
+resource "github_team" "some_team" {
+  name        = var.team_name
+  description = "TF managed team"
+}
+
+resource "github_team_membership" "some_team_membership" {
+  for_each = toset(var.members)
+
+  team_id  = github_team.some_team.id
+  username = each.key
+  role     = "member"
+}
+
+resource "github_team_repository" "some_team_repo" {
+  team_id    = github_team.some_team.id
+  repository = data.github_repository.the_repo.name
+  permission = "push"
+}
+'+
+
---
+++ variables.tf
@@ -0,0 +1,24 @@
+# Copyright YYYY Google LLC
+#
+# This software is provided as-is, without warranty or representation for any
+# use or purpose. Your use of it is subject to your agreement with Google.
+
+variable "team_name" {
+  type = string
+}
+
+variable "members" {
+  type = list
+}
+
+variable "github_org" {
+  type = string
+}
+
+# currently only supports mapping 1 group to 1 repo
+variable "repo_name" {
+  type = string
+}
+
+
+
